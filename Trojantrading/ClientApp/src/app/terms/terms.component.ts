3176f6721143abf1c8e84619f60eab553fa042f0
hillenlin <hillenlin@gmail.com>
2019-04-15 17:56:14 +1000

    Delete useless components

--- Trojantrading/ClientApp/src/app/terms/terms.component.ts
+++ Trojantrading/ClientApp/src/app/terms/terms.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsComponent implements OnInit {
 
+  title:string = "Terms and Conditions";
+
   constructor() { }
 
   ngOnInit() {
